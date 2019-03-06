deae30ebdd9ce6073887479ab5a4872eebb311b9
Edwin Qiu <edwinqiu93@gmail.com>
2019-03-06 04:30:29 -0800

    fetched data from newsapi

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-//L
-    //the-new-york-times
-    //ABC News
-//C
-    //the-wall-street-journal
-    //bloomberg
-//R
-    //fox-news
-    //breitbart-news
+// L
+//     the-new-york-times
+//     abc-news
+// C
+//     the-wall-street-journal
+//     bloomberg
+// R
+//     fox-news
+//     breitbart-news
+
+'use strict'
+
+const sourceArray = ['the-new-york-times', 'abc-news', 'the-wall-street-journal', 'bloomberg', 'fox-news', 'breitbart-news'];
+
+const apiKey = '86b856af98974f2e98d7934417bf165e';
+
+const searchUrl = 'https://newsapi.org/v2/everything';
 
 $(function () {
     console.log('App working. Ready to serve you, developer.')
@@ -18,12 +26,43 @@ function watchForm() {
     $('form.search-button').on('submit', function (e) {
         e.preventDefault();
         let userInput = $('#search-news').val();
-        console.log(userInput);
+        console.log('userInputtttt', userInput);
         getNews(userInput);
     })
 }
 
-function getNews(userInput) {
+function formatString(params) {
+    let result = [];
+    for (let key in params) {
+        result.push(`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    }
+    return result.join('&')
+}
+
+
+function getNews(query) {
+
+    let params = {
+        q: query,
+        sources: "abc-news",
+        lang: "en",
+        sortBy: "relevancy",
+        // pageSize: ,
+        // page: ,
+    };
+
+    let queryString = formatString(params)
+    let url = searchUrl + '?' + queryString;
+    console.log('final url', url)
+
+    let options = {
+        headers: new Headers ({
+            'X-Api-Key': apiKey
+        })
+    }
     
+    fetch(url, options)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson))
 }
 
