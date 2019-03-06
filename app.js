eb3178d39e40b5595c7c8e07c9503d545526559a
Edwin Qiu <edwinqiu93@gmail.com>
2019-03-06 05:48:58 -0800

    displayed results on DOM seperated by source

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 //     abc-news
 // C
 //     the-wall-street-journal
-//     bloomberg
+//     usa-today
 // R
 //     fox-news
 //     breitbart-news
 
 'use strict'
 
-const sourceArray = ['the-new-york-times', 'abc-news', 'the-wall-street-journal', 'bloomberg', 'fox-news', 'breitbart-news'];
+const sourceArray = ['the-new-york-times', 'abc-news', 'the-wall-street-journal', 'usa-today', 'fox-news', 'breitbart-news'];
 
 const apiKey = '86b856af98974f2e98d7934417bf165e';
 
@@ -42,27 +42,55 @@ function formatString(params) {
 
 function getNews(query) {
 
-    let params = {
-        q: query,
-        sources: "abc-news",
-        lang: "en",
-        sortBy: "relevancy",
-        // pageSize: ,
-        // page: ,
-    };
+    for (let i = 0 ; i < sourceArray.length ; i++) {
+        let params = {
+            q: query,
+            sources: `${sourceArray[i]}`,
+            lang: "en",
+            sortBy: "relevancy"
+            // pageSize: ,
+            // page: ,
+        };
 
-    let queryString = formatString(params)
-    let url = searchUrl + '?' + queryString;
-    console.log('final url', url)
+        let queryString = formatString(params)
+        let url = searchUrl + '?' + queryString;
+        console.log('final url', url)
 
-    let options = {
-        headers: new Headers ({
-            'X-Api-Key': apiKey
+        let options = {
+            headers: new Headers ({
+                'X-Api-Key': apiKey
+            })
+        }
+        
+        fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
         })
-    }
-    
-    fetch(url, options)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            alert(`Error message: ${err.message}`)
+        })
+    } 
 }
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $(`#${responseJson.articles[0].source.id}`).empty();
+    //responseJson.articles[0].urlToImage
+
+    $(`#${responseJson.articles[0].source.id}`).append(`<img class="article-image" src="${responseJson.articles[0].urlToImage}" alt="first article's image">`)
+
+    for (let i = 0 ; i < responseJson.articles.length; i++) {
+        $(`#${responseJson.articles[0].source.id}`).append(`
+            <li class="article-list">
+                <h4><a href="${responseJson.articles[i].url}" target="_blank">${responseJson.articles[i].title}</a></h4>
+                <p>${responseJson.articles[i].description}</p>
+            </li>
+        `)
+    }
+}
+
 
